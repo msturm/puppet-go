4ae64ea35fedf0da041f78b1668cd8f218b187b9
Martin Sturm <msturm@ebay.com>
2014-01-13 14:01:36 +0100

    Use fixed chgo which works with zsh

--- manifests/init.pp
+++ manifests/init.pp
@@ -38,7 +38,7 @@ class go(
   repository { $chgo_root:
     ensure => $chgo_version,
     force  => true,
-    source => 'wfarr/chgo',
+    source => 'msturm/chgo',
     user   => $chgo_user,
   }
 
